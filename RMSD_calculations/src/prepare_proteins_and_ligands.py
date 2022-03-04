7cf472cb70d99f012fb0792ba8417344b3f256b0
jonathanwaehrer <jonathan.waehrer@student.uni-tuebingen.de>
2022-03-04 16:28:17 +0100

    added .pdbqt conversion when preparing proteins for Vina.

--- RMSD_calculations/src/prepare_proteins_and_ligands.py
+++ RMSD_calculations/src/prepare_proteins_and_ligands.py
@@ -9,17 +9,18 @@ from openbabel import pybel
 from pymol import cmd
 from tqdm import tqdm
 
+
 # ===================================================== FUNCTIONS ==================================================== #
-def prepare_proteins(pdb_dir: str, lepro_dir: str, system='mac'):
+def prepare_proteins(pdb_dir: str, bin_dir: str, system='mac'):
     """
     LePro automatically adds hydrogen by considering protonation state of histidine. Also removes all crystal water,
     ions, small ligands and cofactors. While LePro was especially designed especially for subsequent use of LeDock,
-    the prepared proteins can also be used by autodock vina and smina.
+    the prepared proteins can also be used by Autodock vina and smina.
 
     Parameters
     ----------
     pdb_dir: Directory containing an arbitrary amount of .pdb files for preparation.
-    lepro_dir: Path to lepro binary executable.
+    bin_dir: Binary executables.
     system: Operating system. Choose mac (default) or linux.
     """
     # ---- Check input system (LePro is only available for mac and linux) ---- #
@@ -29,7 +30,7 @@ def prepare_proteins(pdb_dir: str, lepro_dir: str, system='mac'):
 
     # ---- Get file paths, create output directory ---- #
     pdb_dir = os.path.dirname(pdb_dir)
-    lepro_dir = os.path.dirname(lepro_dir)
+    bin_dir = os.path.dirname(bin_dir)
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
     output_dir = current_dir + "/../out/prepared_proteins"
@@ -38,21 +39,33 @@ def prepare_proteins(pdb_dir: str, lepro_dir: str, system='mac'):
 
     # ---- Iteratively use LePro (either for mac or linux) on pdb files---- #
     if system == 'mac':
-        for file in tqdm(os.listdir(pdb_dir), desc="...preparing receptors"):
-            if file.endswith(".pdb"):
-                os.system("%s/lepro_mac %s" % (lepro_dir, file))
-                os.system("mv pro.pdb %s/%s" % (output_dir, file))  # LePro always saves output as 'pro.pdb'
+        for pdb in tqdm(os.listdir(pdb_dir), desc="...preparing receptors...                      "):
+            file = os.path.join(pdb_dir, pdb)
+            if pdb.endswith(".pdb"):
+                os.system("%s/lepro_mac %s" % (bin_dir, file))
+                os.system("mv pro.pdb %s/%s" % (output_dir, pdb))  # LePro always saves output as 'pro.pdb'
     else:
-        for file in tqdm(os.listdir(pdb_dir)):
-            if file.endswith(".pdb"):
-                os.system("%s/lepro_mac %s" % (lepro_dir, file))
-                os.system("mv pro.pdb %s/%s" % (output_dir, file))  # LePro always saves output as 'pro.pdb'
+        for pdb in tqdm(os.listdir(pdb_dir)):
+            file = os.path.join(pdb_dir, pdb)
+            if pdb.endswith(".pdb"):
+                os.system("%s/lepro_mac %s" % (bin_dir, file))
+                os.system("mv pro.pdb %s/%s" % (output_dir, pdb))  # LePro always saves output as 'pro.pdb'
     os.system("rm dock.in")
+
+    # ---- Vina requires .pdbqt files for docking (AutoDock provides a tool for that) ---- #
+    for pdb in tqdm(os.listdir(output_dir), desc="...adding charges and atom types for Vina...   "):
+        if pdb.endswith(".pdb"):
+            file = os.path.join(output_dir, pdb)
+            os.system("%s/prepare_receptor -r %s -o %s/%sqt >/dev/null 2>&1" % (bin_dir, file, output_dir, pdb))
+
+    '''t1 = "/Users/jonathanwahrer/Desktop/Msc/3_Semester/Drug_Design_Praktikum/waehrer_HTS_screening/RMSD_calculations/out/prepared_proteins/2jam.pdb"
+    t2 = "/Users/jonathanwahrer/Desktop/Msc/3_Semester/Drug_Design_Praktikum/waehrer_HTS_screening/RMSD_calculations/out/prepared_proteins/2jam.pdbqt"
+    os.system("%s/prepare_receptor -r %s -o %s" % (bin_dir, t1, t2))'''
 
 
 def prepare_ligands(pdb_dir):
     """
-    Uses Open Babel's Pybel to add hydrogens to ligands
+    Uses Open Babel's Pybel to add hydrogens to ligands.
 
     Parameters
     ----------
@@ -67,8 +80,13 @@ def prepare_ligands(pdb_dir):
         os.mkdir(output_dir)
 
     # ---- get ligands from chain A of pdb files ---- #
+    for pdb in tqdm(os.listdir(pdb_dir), desc="...extracting ligands from proteins...         "):
+        '''cmd.load(pdb)
+        cmd.select("sele_ligand", "organic and chain A")
+        cmd.save("%s/%s_ligand.mol2" % (pdb, output_dir), format='mol2', selection='sele_ligand')
+        cmd.reinitialize()'''
 
 
 def run(input_proteins, path_to_bin):
-    prepare_proteins(pdb_dir=input_proteins, lepro_dir=path_to_bin, system="mac")
+    prepare_proteins(pdb_dir=input_proteins, bin_dir=path_to_bin, system="mac")
     prepare_ligands(pdb_dir=input_proteins)
